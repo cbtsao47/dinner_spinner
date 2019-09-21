7cb4b8182575e5065b99917ecffb382e08d2d8ef
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-09-21 18:57:25 -0500

    First draft version

--- src/components/RestaurantInfo.js
+++ src/components/RestaurantInfo.js
@@ -5,20 +5,44 @@ const RestaurantInfo = ({ selected }) => {
   // const selectedItem = JSON.parse(selected);
   console.dir(selected.image_url);
   return (
-    <div className="restaurant-info-container">
-      <div className="restaurant-img-parent">
-        <img
-          className="restaurant-image"
-          src={selected.image_url}
-          alt={selected.name}
-        />
-      </div>
-      {/* <p>{selected.is_closed}</p>
+    <div className="restaurant-container">
+      <div className="restaurant-info-container">
+        <div className="restaurant-img-parent">
+          <img
+            className="restaurant-image"
+            src={selected.image_url}
+            alt={selected.name}
+          />
+        </div>
+        <div className="restaurant-content">
+          <div className="restaurant-title">
+            <h2>My pizza</h2>
+            <div>
+              {selected.is_closed && <p>closed now</p>}
+              {!selected.is_closed && <p>open now</p>}
+            </div>
+          </div>
+          <div className="restaurant-address">
+            <p>{selected.location && selected.location.display_address}</p>
+          </div>
+
+          <div className="restaurant-phone">
+            <p>{selected.phone}</p>
+          </div>
+
+          <div className="restaurant-distance">
+            <p>{Math.ceil(selected.distance / 1610)} mile(s) away</p>
+          </div>
+
+          <div className="restaurant-reserve">
+            <p>Reserve</p>
+          </div>
+        </div>
+        {/* 
       <p>{selected.rating}</p>
-      <address>
-        <p>{selected.location && selected.location.display_address}</p>
-      </address>
+
       <a href={`tel:${selected.phone}`}>{selected.display_phone}</a> */}
+      </div>
     </div>
   );
 };
