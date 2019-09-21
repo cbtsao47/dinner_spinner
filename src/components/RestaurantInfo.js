59e2ec41fc5768c785beff795dda137c865ee7d6
Lambda_School_Loaner_62 <lambda_school_loaner_62@Lambda-School-Loaner-62s-MacBook-Pro.local>
2019-09-21 16:28:53 -0500

    fix restaurant info page

--- src/components/RestaurantInfo.js
+++ src/components/RestaurantInfo.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 const RestaurantInfo = ({ selected }) => {
+  console.log(selected);
   return (
     <div>
       <img src={selected.image_url} alt={selected.name} />
       <p>{selected.is_closed}</p>
       <p>{selected.rating}</p>
       <address>
-        <p>{selected.location.display_address}</p>
+        <p>{selected.location && selected.location.display_address}</p>
       </address>
       <a href={`tel:${selected.phone}`}>{selected.display_phone}</a>
     </div>
