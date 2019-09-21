089b67a69c1c722ebf7fdf4f928607bf118ca669
Lambda_School_Loaner_62 <lambda_school_loaner_62@Lambda-School-Loaner-62s-MacBook-Pro.local>
2019-09-21 10:48:07 -0500

    component set up

---
+++ src/components/RestaurantInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class RestaurantInfo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>RestaurantInfo</h1>
+      </div>
+    );
+  }
+}
+
+export default RestaurantInfo;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Spinner from '../components/Spinner'
+
+export default class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                <Spinner/>
+            </div>
+        )
+    }
+}
