20722f426ac54126d86aea95b599fd7a348dd6e6
Lambda_School_Loaner_62 <lambda_school_loaner_62@Lambda-School-Loaner-62s-MacBook-Pro.local>
2019-09-21 10:49:42 -0500

    add component layout

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react'
-import Spinner from '../components/Spinner'
-
+import React, { Component } from "react";
+import Spinner from "../components/Spinner";
+import RestaurantInfo from "../components/RestaurantInfo";
+import Button from "../components/Button";
 export default class LandingPage extends Component {
-    render() {
-        return (
-            <div>
-                <Spinner/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Spinner />
+        <Button />
+        <RestaurantInfo />
+      </div>
+    );
+  }
 }
